02bac1756b9a6ac0b2f32c2e41b55e100ee51390
elebor <elebor@debian.elebor>
2025-01-31 21:47:45 +0300

    WTF xD

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -4,45 +4,40 @@ import Control.Monad
 import Helpers.Parser
 import Helpers.Types
 import Data.Map as M
+import Data.Char (toLower)
 
 data Env = Env
         {
             globals :: M.Map String SExpr
-        } deriving (Show, Eq)
+        } deriving Show
 
-env :: Env
-env = Env { globals = M.fromList [("x", Number 10), ("y", Number 20)] }
+initialEnv :: Env
+initialEnv = Env { globals = M.fromList [("x", Number 10), ("y", Number 20)] }
 
 ---- Функция eval
 eval :: Env -> SExpr -> (SExpr, Env)
-eval _ (Number n) = trace ("Evaluating Number: " ++ show n) $ (Number n, env)
-
-eval _ (StringLiteral s) = trace ("Evaluating StringLiteral: " ++ show s) $ (StringLiteral s, env)
-
-eval _ (Bool b) = trace ("Evaluating Bool: " ++ show b) $ (Bool b, env)
-
-eval _ Nil = trace "Evaluating Nil" (Nil, env)
+eval env (Number n) = (Number n, env)
+eval env (StringLiteral s) = (StringLiteral s, env)
+eval env (Bool b) = (Bool b, env)
+eval env Nil = (Nil, env)
 
 eval env (Atom var) = trace ("Evaluating Atom: " ++ var) $
     case M.lookup var (globals env) of
         Just val -> trace ("Found value for " ++ var ++ ": " ++ show val) (val, env)
         Nothing  -> error $ "Variable not found: " ++ var
 
-eval env (Cons car cdr) = trace "Evaluating Cons" $
+eval env (Cons car cdr) = trace "[EVAL] Cons" $
     let (carResult, env1) = eval env car
         (cdrResult, env2) = eval env1 cdr
     in (Cons carResult cdrResult, env2)
 
-eval env (ArithOp op args) = trace ("Evaluating ArithOp: " ++ [op]) $
+eval env (ArithOp op args) = 
     let evalArgs = Prelude.map (eval env) args
-        _ = trace ("Evaluated arguments: " ++ show evalArgs) ()
         nums = Prelude.map toNumber evalArgs
-        _ = trace ("Converted to numbers: " ++ show nums) ()
         result = Number $ applyArithOp op nums
-        _ = trace ("Result of arithmetic operation: " ++ show result) ()
     in (result, env)
   where
-    toNumber (Number n, env1) = n
+    toNumber (Number n, _) = n
     toNumber expr = error $ "Comparison operations expect numbers, but got: " ++ show expr
 
     applyArithOp '+' = sum
@@ -50,16 +45,13 @@ eval env (ArithOp op args) = trace ("Evaluating ArithOp: " ++ [op]) $
     applyArithOp '*' = product
     applyArithOp '/' = foldl1 div
 
-eval env (CompareOp op args) = trace ("Evaluating CompareOp: " ++ op) $
+eval env (CompareOp op args) = 
     let evalArgs = Prelude.map (eval env) args
-        _ = trace ("Evaluated arguments: " ++ show evalArgs) ()
         nums = Prelude.map toNumber evalArgs
-        _ = trace ("Converted to numbers: " ++ show nums) ()
         result = Bool $ applyCompareOp op nums
-        _ = trace ("Result of comparison operation: " ++ show result) ()
     in (result, env)
   where
-    toNumber (Number n, env) = n
+    toNumber (Number n, _) = n
     toNumber _ = error "Comparison operations expect numbers"
 
     applyCompareOp "<"  = \[x, y] -> x < y
@@ -70,75 +62,155 @@ eval env (CompareOp op args) = trace ("Evaluating CompareOp: " ++ op) $
     applyCompareOp "!=" = \[x, y] -> x /= y
     applyCompareOp _    = error "Unsupported comparison operator"
 
-eval env (If cond thenBranch elseBranch) = trace "Evaluating If" $
+eval env (If cond thenBranch elseBranch) = 
     case eval env cond of
-        (Bool True, env1) -> trace "Condition is true, evaluating thenBranch" $ eval env thenBranch
-        (Bool False, env2) -> trace "Condition is false, evaluating elseBranch" $ eval env elseBranch
+        (Bool True, env1) -> eval env1 thenBranch
+        (Bool False, env2) -> eval env2 elseBranch
         _ -> error "Condition must be a boolean"
 
-eval env (LogicBinary op args) = trace ("Evaluating LogicBinary: " ++ show op) $
+eval env (LogicBinary op args) = 
     let evalArgs = Prelude.map (eval env) args
-        _ = trace ("Evaluated arguments: " ++ show evalArgs) ()
         bools = Prelude.map toBool evalArgs
-        _ = trace ("Converted to booleans: " ++ show bools) ()
         result = Bool $ applyLogicBinaryOp op bools
-        _ = trace ("Result of logic binary operation: " ++ show result) ()
     in (result, env)
   where
-    toBool (Bool b, env) = b
+    toBool (Bool b, _) = b
     toBool _ = error "Logic operations expect booleans"
 
     applyLogicBinaryOp And = and
     applyLogicBinaryOp Or  = or
 
-eval env (LogicUnary op arg) = trace ("Evaluating LogicUnary: " ++ show op) $
+eval env (LogicUnary op arg) = 
     case eval env arg of
-        (Bool b, newEnv) -> trace ("Evaluated argument: " ++ show b) $ (Bool $ applyLogicUnaryOp op b, env)
+        (Bool b, newEnv) -> (Bool $ applyLogicUnaryOp op b, newEnv)
         _ -> error "Logic operations expect booleans"
   where
     applyLogicUnaryOp Not = not
 
+eval env (Quote expr) = trace ("[EVAL] Quote got: " ++ show expr) $ (expr, env)
 
-eval env (Quote expr) = trace ("Evaluating Quote: " ++ show expr) (expr, env)
-
-eval env (Define var expr) = trace ("Evaluating Define: " ++ var) $
+eval env (Define var expr) = 
     let (val, env1) = eval env expr
-        _ = trace ("Evaluated expression: " ++ show val) ()
-        newEnv = env { globals = M.insert var val (globals env1) }
-        _ = trace ("Inserted VAL: " ++ show val) ()
-
+        newEnv = env1 { globals = M.insert var val (globals env1) }
     in (val, newEnv)
 
-eval env (Var var expr) = trace ("Evaluating Var: " ++ var) $
+eval env (Var var expr) = 
     let (result, newEnv) = eval env expr
         updatedGlobals = M.insert var result (globals env)
         updatedEnv = newEnv {globals = updatedGlobals}
     in (result, updatedEnv)
 
-eval env (Lambda params body) = trace ("Evaluating Lambda: " ++ show params) $ (Lambda params body, env)
+eval env (Lambda params body) = (Lambda params body, env)
+
+eval env (StringCheck expr) = 
+    case eval env expr of
+        (StringLiteral _, env1) -> (Bool True, env1)
+        _ -> (Bool False, env)
+
+eval env (StringEqual expr1 expr2) = trace "Evaluating StringEqual" $
+    case (eval env expr1, eval env expr2) of
+        ((StringLiteral s1, env1), (StringLiteral s2, env2)) -> (Bool (s1 == s2), env2)
+        _ -> error "string=? expects two strings"
+
+eval env (Substring strExpr startExpr endExpr) = trace "Evaluating Substring" $
+    case (eval env strExpr, eval env startExpr, eval env endExpr) of
+        ((StringLiteral s, env1), (Number start, env2), (Number end, env3)) ->
+            if start >= 0 && end <= toInteger (length s) && start <= end
+                then (StringLiteral (Prelude.take (fromInteger (end - start)) (Prelude.drop (fromInteger start) s)), env3)
+                else error "Invalid indices for substring"
+        _ -> error "substring expects a string and two numbers"
+
+eval env (StringAppend exprs) = trace "Evaluating StringAppend" $
+    let (results, env1) = Prelude.foldl (\(acc, env') expr ->
+            case eval env' expr of
+                (StringLiteral s, env'') -> (acc ++ [s], env'')
+                _ -> error "string-append expects strings"
+            ) ([], env) exprs
+    in (StringLiteral (concat results), env1)
+
+eval env (Car expr) = 
+    case eval env expr of
+        (Cons car _, env1) -> (car, env1)
+        (atom, env1) -> (atom, env1)
+
+eval env (Cdr expr) = 
+    case eval env expr of
+        (Cons _ cdr, env1) -> (cdr, env1)
+        (_, env1) -> (Nil, env1)
+
+eval env (Atom var) 
+    | all (`elem` "ad") (Prelude.map toLower var) = 
+        case M.lookup var (globals env) of
+            Just val -> trace ("Found value for " ++ var ++ ": " ++ show val) (val, env)
+            Nothing  -> error $ "Variable not found: " ++ var
+    | otherwise = 
+        case M.lookup var (globals env) of
+            Just val -> trace ("Found value for " ++ var ++ ": " ++ show val) (val, env)
+            Nothing  -> error $ "Variable not found: " ++ var
+
+eval env (Cons (Atom var) expr) 
+    | all (`elem` "ad") (Prelude.map toLower var) = 
+        let (result, env1) = eval env expr
+        in (makeCarCdrFunc var result, env1)
+    | otherwise = 
+        let (carResult, env1) = eval env (Atom var)
+            (cdrResult, env2) = eval env1 expr
+        in (Cons carResult cdrResult, env2)
 
 eval _ expr = error $ "Unsupported expression: " ++ show expr
 
+applyCarCdr :: String -> SExpr -> SExpr
+applyCarCdr [] expr = expr
+applyCarCdr (c:cs) expr = 
+    case toLower c of
+        'a' -> applyCarCdr cs (Car expr)
+        'd' -> applyCarCdr cs (Cdr expr)
+        _   -> error $ "Invalid function name: " ++ (c:cs)
+
+makeCarCdrFunc :: String -> SExpr -> SExpr
+makeCarCdrFunc name expr = 
+    if all (`elem` "ad") (Prelude.map toLower name)
+        then applyCarCdr (reverse name) expr
+        else error $ "Invalid function name: " ++ name
+
+
+-- test :: IO ()
+-- processLine :: Env -> String -> IO Env
+-- processLine curEnv line = do
+--     let newEnv = curEnv { globals = M.insert line (Number 2) (globals curEnv) }
+--     putStrLn $ "Processing line: " ++ line
+--     putStrLn $ "New Env: " ++ show newEnv
+--     return newEnv
+
+-- initialEnv = Env { globals = M.empty }
+
 test :: IO ()
 test = do
-    let initialEnv = env
+    -- let initialEnv = initialEnv
     let filename = "test.txt"
     content <- readFile filename
     let linesOfContent = lines content
     finalEnv <- foldM processLine initialEnv linesOfContent
-    putStrLn $ "Final environment: " ++ show finalEnv
+    print "end."
+    -- putStrLn $ "Final environment: " ++ show finalEnv
 
 processLine :: Env -> String -> IO Env
-processLine env line = do
+processLine curEnv line = do
+    -- print "CUR ENV:"
+    -- print curEnv
+    -- print "TOKENS:"
+    -- putStrLn ("[PROCESS LINE CUR ENV CHECK]: " ++ show curEnv)
     let tokens = tokenize line
+    print tokens
     case runParser parseSExpr tokens of
         Left err -> do
             putStrLn $ "Parse error: " ++ err
-            return env
+            return curEnv
         Right (rest, expr) -> do
             putStrLn "\n"
-            putStrLn $ "Parsed expression: " ++ show expr
-            putStrLn "EVALUATING"
-            let (result, newEnv) = eval env expr
-            putStrLn $ "Result: " ++ show result
+            -- putStrLn $ "Parsed expression: " ++ show expr
+            -- putStrLn "EVALUATING"
+            let (result, newEnv) = eval curEnv expr
+            putStrLn $ "[PROCESS LINE] Result: " ++ show result
+            -- putStrLn $ "[PROCESS LINE] Env: " ++ show newEnv
             return newEnv
